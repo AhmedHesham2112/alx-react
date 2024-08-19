0968c1ba22b4cc07899cb7f337e50ca33ee6d808
AhmedHesham2112 <AhmedHesham2112>
2024-08-19 16:07:57 +0300

    task_2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: "production",
+  performance: {
+    maxAssetSize: 1000000, // Adjust size limit as needed
+  },
+};
